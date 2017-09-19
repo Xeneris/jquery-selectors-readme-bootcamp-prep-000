9f4851c12e976a4f552ee11cff2f3af05c3ebce8
Davin Dillon <davindillon@gmail.com>
2017-09-19 01:16:27 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -10,3 +10,9 @@ function lastImageSelector(){
 function ninjaBabySelector(){
   return $('#baby-ninja');
 }
+function divSelector(){
+  return $('.pics');
+}
+function firstListItem(){
+  return $('ul li:first-child');
+}
